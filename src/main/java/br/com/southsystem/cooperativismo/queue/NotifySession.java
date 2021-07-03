49faca4596d556cbbd54908fe4beb23b2f14e266
Mário Sérgio <mariosergio952@hotmail.com.br>
2021-07-03 19:34:31 -0300

    - add unit test partial

--- src/main/java/br/com/southsystem/cooperativismo/queue/NotifySession.java
+++ src/main/java/br/com/southsystem/cooperativismo/queue/NotifySession.java
@@ -1,6 +1,7 @@
 package br.com.southsystem.cooperativismo.queue;
 
 import br.com.southsystem.cooperativismo.domain.dto.ResultVote;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log4j2
 public class NotifySession {
 
     @Autowired
@@ -18,6 +20,11 @@ public class NotifySession {
     private Queue queue;
 
     public void finishSession(ResultVote resultSession) {
-        rabbitTemplate.convertAndSend(this.queue.getName(), resultSession);
+        try {
+            rabbitTemplate.convertAndSend(this.queue.getName(), resultSession);
+        } catch (Exception e) {
+            log.info("Erro ao envia notificação", e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/br/com/southsystem/cooperativismo/service/ScheduleServiceTest.java
+++ src/test/java/br/com/southsystem/cooperativismo/service/ScheduleServiceTest.java
@@ -1,10 +1,86 @@
 package br.com.southsystem.cooperativismo.service;
 
+import br.com.southsystem.cooperativismo.domain.model.Schedule;
+import br.com.southsystem.cooperativismo.domain.request.ScheduleRequest;
+import br.com.southsystem.cooperativismo.exception.NotFoundException;
+import br.com.southsystem.cooperativismo.repostory.ScheduleRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class ScheduleServiceTest {
 
+    @Mock
+    private ScheduleRepository scheduleRepository;
 
+    @Mock
+    private ScheduleService scheduleService;
+
+    @Test
+    public void registerNewScheduleTest() {
+        ScheduleRequest scheduleRequest = new ScheduleRequest();
+        scheduleRequest.setTitle("Exemple title");
+
+        Schedule schedule = new Schedule(scheduleRequest.getTitle());
+
+        when(scheduleRepository.save(schedule)).thenReturn(schedule);
+
+        Schedule response = scheduleService.registerNewSchedule(scheduleRequest);
+
+        Assert.assertNull(response);
+
+    }
+
+    @Test
+    public void registerNewScheduleErrorTest() {
+        ScheduleRequest scheduleRequest = new ScheduleRequest();
+        scheduleRequest.setTitle("Ex");
+
+        Schedule schedule = new Schedule(scheduleRequest.getTitle());
+
+        when(scheduleRepository.save(schedule)).thenThrow(new DataIntegrityViolationException(""));
+
+        scheduleService.registerNewSchedule(scheduleRequest);
+
+        verify(scheduleRepository, never()).save(schedule);
+    }
+
+    @Test
+    public void findByIdTest() {
+        Long scheduleId = 1L;
+        when(scheduleRepository.findById(scheduleId)).thenReturn(Optional.of(getSchedule()));
+
+        scheduleService.findById(scheduleId);
+
+        verify(scheduleService).findById(scheduleId);
+    }
+
+    @Test
+    public void findByIdErrorTest() {
+        Long scheduleId = 1L;
+        when(scheduleRepository.findById(scheduleId)).thenThrow(new NotFoundException(""));
+
+        verify(scheduleService, never()).findById(scheduleId);
+    }
+
+    public Schedule getSchedule() {
+        Schedule schedule = new Schedule();
+        schedule.setTitle("Teste");
+        schedule.setCreatedAt(LocalDateTime.now());
+        schedule.setId(1L);
+        return schedule;
+    }
 }
